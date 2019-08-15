65117f6039a76568ae05161d9700a0a3c9773ac2
super0kontik <50371760+super0kontik@users.noreply.github.com>
2019-08-15 12:16:33 +0300

    Update index.js

--- resolvers/index.js
+++ resolvers/index.js
@@ -7,27 +7,3 @@ module.exports={
     ...bookingResolvers,
     ...eventResolvers
 };
-
-// mutation{
-//     cancelBooking(bookingId:"5d2f34e9ca1b8e59fb11b42e"){
-//         title
-//         creator{
-//             email
-//             createdEvents{
-//                 title
-//             }
-//         }
-//     }
-// }
-
-// mutation{
-//     bookEvent(eventId:"5d2ef15e45fb632e61ce7469"){
-//         user{
-//             email
-//         }
-//         event{
-//             title
-//         }
-//         createdAt
-//     }
-// }
